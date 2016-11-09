a43854a8994b5690c502e135a64bdd75dec09fea
Mo Bloggs <mo.omar.sharif@gmail.com>
2016-11-09 09:44:03 +0000

    Added rc-time-picker

--- public/src/js/components/timesheet.jsx
+++ public/src/js/components/timesheet.jsx
@@ -4,47 +4,48 @@ var React = require("react");
 var ReactBsTable = require("react-bootstrap-table");
 var BootstrapTable = ReactBsTable.BootstrapTable;
 var TableHeaderColumn = ReactBsTable.TableHeaderColumn;
+var TimePicker = require('rc-time-picker');
 
-var products = [
-	  {
-	      id: 1,
-	      name: "Product1",
-	      price: 120
-	  },{
-	      id: 2,
-	      name: "Product2",
-	      price: 80
-	  },{
-	      id: 3,
-	      name: "Product3",
-	      price: 207
-	  },{
-	      id: 4,
-	      name: "Product4",
-	      price: 100
-	  },{
-	      id: 5,
-	      name: "Product5",
-	      price: 150
-	  },{
-	      id: 6,
-	      name: "Product1",
-	      price: 160
-	  }
-	];
+var week = [
+  {
+      id: 1,
+      name: "timeIn"
+
+  },{
+      id: 2,
+      name: "timeOut"
+  }
+];
 
 var Timesheet = React.createClass({
+	handleSubmit: function(){
+		console.log("submitted!!");
+	},
+	handleTime: function(){
+		<TimePicker
+    		style={{ width: 100 }}
+    		defaultValue={moment()}
+  		/>
+	},
 	render: function(){
 		return (
 			<div className="timesheetEntery">
-				 <BootstrapTable data={products} striped={true} hover={true}>
-				      <TableHeaderColumn isKey={true} dataField="id">Product ID</TableHeaderColumn>
-				      <TableHeaderColumn dataField="name">Product Name</TableHeaderColumn>
-				      <TableHeaderColumn dataField="price">Product Price</TableHeaderColumn>
+				 <BootstrapTable className="tableData" data={week} striped={true} hover={true}>
+				  	  <TableHeaderColumn isKey={true} dataField="name"></TableHeaderColumn>
+				 	  <TableHeaderColumn dataField={this.handleTime}>Mon</TableHeaderColumn>
+				      <TableHeaderColumn dataField="day">Tue</TableHeaderColumn>
+				      <TableHeaderColumn dataField="price">Wed</TableHeaderColumn>
+				      <TableHeaderColumn dataField="price">Thu</TableHeaderColumn>
+				      <TableHeaderColumn dataField="price">Fri</TableHeaderColumn>
+				      <TableHeaderColumn dataField="price">Sat</TableHeaderColumn>
+				      <TableHeaderColumn dataField="price">Sun</TableHeaderColumn>
 				  </BootstrapTable>
+				  <button onClick={this.handleSubmit}>save</button>
 			</div>	
 		)
 	}
 });
+
+ // <TableHeaderColumn isKey={true} dataField="id">Mon</TableHeaderColumn>
 
 module.exports = Timesheet;
